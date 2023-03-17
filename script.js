4ddef42df410e3807a64e390ccd399c7f21a1886
alexdilion <alexdilion@protonmail.com>
2023-03-17 23:07:48 +0000

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  if (y === 0) {
+    console.error("Attempted to divide by 0");
+    return "error"
+  }
+
+  return x / y;
+}
