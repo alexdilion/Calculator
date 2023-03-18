abd9cece15ddd3f27f15cb0ded8ccacecd0b1716
alexdilion <alexdilion@protonmail.com>
2023-03-18 00:17:48 +0000

    Create html layout for page

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const OPERATIONS = {
-  add,
-  subtract,
-  multiply,
-  divide
-}
+    add,
+    subtract,
+    multiply,
+    divide,
+};
 
 function add(x, y) {
-  return x + y;
+    return x + y;
 }
 
 function subtract(x, y) {
-  return x - y;
+    return x - y;
 }
 
 function multiply(x, y) {
-  return x * y;
+    return x * y;
 }
 
 function divide(x, y) {
-  if (y === 0) {
-    console.error("Attempted to divide by 0");
-    return "error"
-  }
+    if (y === 0) {
+        console.error("Attempted to divide by 0");
+        return "error";
+    }
 
-  return x / y;
+    return x / y;
 }
 
 function operate(x, y, operation) {
-  return OPERATIONS[operation](x, y)
+    return OPERATIONS[operation](x, y);
 }
