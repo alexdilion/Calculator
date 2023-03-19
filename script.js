55a4fc80560283b39b66d9793a5d64ce3f8c3170
alexdilion <alexdilion@protonmail.com>
2023-03-19 22:46:49 +0000

    Redo logic for app, make operate function

--- script.js
+++ script.js
@@ -5,12 +5,26 @@ const OPERATIONS = {
     divide,
 };
 
-const equationText = document.querySelector("#equation");
-const resultText = document.querySelector("#result")
-const clearButton = document.querySelector("#clear");
-const numberButtons = document.querySelectorAll(".number");
+const OPERATOR_SIGNS = {
+    add: "+",
+    subtract: "-",
+    multiply: "×",
+    divide: "÷",
+};
 
-let displayValue = "";
+const equationText = document.querySelector("#equation");
+const resultText = document.querySelector("#result");
+const clearButton = document.querySelector("#clear");
+const equalsButton = document.querySelector("#operate");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+
+let firstTerm = "";
+let secondTerm = "";
+let currentOperator = "";
+let result = "";
+let newEquation = true;
+let onFirstTerm = true;
 
 function add(x, y) {
     return x + y;
@@ -25,37 +39,112 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
-    if (y === 0) {
-        console.error("Attempted to divide by 0");
-        return "error";
-    }
-
     return x / y;
 }
 
-function operate(x, y, operation) {
-    return OPERATIONS[operation](x, y);
+function updateDisplay() {
+    equationText.textContent = firstTerm;
+
+    if (currentOperator !== "") {
+        equationText.textContent += ` ${OPERATOR_SIGNS[currentOperator]}`;
+    }
+
+    if (secondTerm !== "") {
+        equationText.textContent += ` ${secondTerm}`;
+    }
+
+    if (result !== "") {
+        equationText.textContent += " =";
+        resultText.textContent = result;
+    }
+
+    if (result === "") {
+        resultText.textContent = "";
+    }
 }
 
-function updateEquation(value) {
-    equationText.textContent += value;
-    displayValue = equationText.textContent;
-}
+function clear() {
+    firstTerm = "";
+    secondTerm = "";
+    currentOperator = "";
+    result = "";
+    newEquation = true;
+    onFirstTerm = true;
 
-function updateResult(result) {
-    resultText.textContent = result;
-}
-
-function clearDisplay() {
-    equationText.textContent = "";
     resultText.textContent = "";
+    equationText.textContent = "";
+}
+
+function onNumberPressed(number) {
+    number = number.target.getAttribute("data-value");
+
+    if (result) clear();
+
+    if (onFirstTerm) {
+        firstTerm += number;
+    } else {
+        secondTerm += number;
+    }
+
+    updateDisplay();
+}
+
+function onOperatorPressed(operator) {
+    operator = operator.target.getAttribute("data-value");
+    currentOperator = operator;
+
+    if (newEquation) {
+        onFirstTerm = false;
+    } else {
+        firstTerm = result;
+        secondTerm = "";
+        result = "";
+    }
+
+    updateDisplay();
+}
+
+function operate() {
+    let x, y;
+
+    if (firstTerm === "" && secondTerm === "") return;
+
+    if (result === "") {
+        x = Number(firstTerm);
+    } else {
+        x = Number(result);
+        firstTerm = result;
+        updateDisplay();
+    }
+
+    y = Number(secondTerm);
+
+    if (y === 0 && currentOperator === "divide") {
+        alert("Error! You tried to divide by 0!");
+        secondTerm = "";
+        updateDisplay();
+        return;
+    }
+
+    if (currentOperator === "") {
+        result = firstTerm;
+        newEquation = false;
+        updateDisplay();
+        return;
+    }
+
+    result = OPERATIONS[currentOperator](x, y).toString();
+    newEquation = false;
+    updateDisplay();
 }
 
 numberButtons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        const value = btn.getAttribute("data-number");
-        updateEquation(value);
-    });
+    btn.addEventListener("click", onNumberPressed);
 });
 
-clearButton.addEventListener("click", clearDisplay);
+operatorButtons.forEach((btn) => {
+    btn.addEventListener("click", onOperatorPressed);
+});
+
+equalsButton.addEventListener("click", operate);
+clearButton.addEventListener("click", clear);
